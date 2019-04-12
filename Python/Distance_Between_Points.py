f246e40c915ccca14d04766f1c8210f47b02a932
robbyhecht <robbyhecht@gmail.com>
2019-04-12 16:34:40 -0400

    added wolf and points distance python exercises

---
+++ Python/Distance_Between_Points.py
@@ -0,0 +1,15 @@
+# Given two ordered pairs calculate the distance between them. Round to two decimal places.
+
+import math
+
+def distance(x1, y1, x2, y2):
+    # Your code here
+    run = x2 - x1
+    rise = y2 - y1
+    dist = round(math.sqrt(run ** 2 + rise ** 2), 2)
+    
+    return dist
+
+
+print(distance(1, 1, 0, 0)) #1.41
+# test.assert_equals(distance(1, 1, 0, 0), 1.41)
